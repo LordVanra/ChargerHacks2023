63092d9b53053fcb900239414a2b04da03b0952c
LordVanra <146479458+LordVanra@users.noreply.github.com>
2023-10-14 14:50:26 -0400

    final commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import sys
 import math
 from pygame.locals import *
+from pygame.sprite import *
 from random import randint
 from time import sleep
 
@@ -14,10 +15,9 @@ windowRect = windowSurface.get_rect()
 
 #colors and fonts
 BLACK = (0, 0, 0)
-LIGHT_GREY = (240, 240, 240)
-basicFont = pygame.font.SysFont(None, 48)
-
-windowSurface.fill(LIGHT_GREY)
+RED = (255, 0, 0)
+basicFont = pygame.font.SysFont(None, 72)
+smallFont = pygame.font.SysFont(None, 32)
 
 #header
 text = basicFont.render("Catch the plastic", False, BLACK)
@@ -26,15 +26,16 @@ textRect.centerx = windowRect.centerx
 textRect.midtop = windowRect.midtop
 
 #backdrop
-image = pygame.image.load("img/bg.bmp")
+image = pygame.image.load("img/bg.png")
 bgRect = image.get_rect()
 bgRect.centerx = windowRect.centerx
 bgRect.centery = windowRect.centery
 
 #player
-plr = pygame.image.load("img/player.bmp")
+plr = pygame.image.load("img/player.png")
 plrRect = plr.get_rect()
 plrRect.midbottom = windowRect.midbottom
+plrRect.y -= 75
 
 #draw items
 windowSurface.blit(image, bgRect)
@@ -44,12 +45,35 @@ windowSurface.blit(plr, plrRect)
 #draw everything
 pygame.display.update()
 
+class Trash(Sprite):
+	def __init__(this, images):
+		super().__init__()
+		this.screen = windowSurface
+		
+		this.trash = pygame.image.load("img/trash/bottle.png")
+		this.rect = this.trash.get_rect()
+		this.rect.x = randint(30, 900)
+		this.rect.y -= 50
+		#rn = randint(0, len(images))
+		
+		#print(rn)
+		
+	def update(this):	
+		this.rect.y += 5
 
 async def main():
 
-	SPEED = 1
+	#constants and variable
+	IMAGES = ["src/trash/bottle.png", "src/trash/bag.png", "src/trash/fork.png", "src/trash/milk.png", "src/trash/trash.png"]
+	SPEED = 7.5
+	MAXRIGHT = 919
+	diff = 250
+	lives = 9
+	score = 0
+	alive = True
 	left = False;
 	right = False;
+	trashes = pygame.sprite.Group()
 	
 	while True:
 		
@@ -75,18 +99,71 @@ async def main():
 				elif event.key == K_LEFT:
 					left = False
 		
-		#move
-		if left:
-			plrRect.x -= SPEED
-		if right:
-			plrRect.x += SPEED
+		if alive:
+		
+			#move
+			if left:
+				plrRect.x -= SPEED
+			if right:
+				plrRect.x += SPEED
 			
-		#redraw player
-		windowSurface.blit(image, bgRect)
-		windowSurface.blit(text, textRect)
-		windowSurface.blit(plr, plrRect)
-		pygame.display.update()
+			if plrRect.x < 0 :
+				plrRect.x = 0
+			if plrRect.x > MAXRIGHT:
+				plrRect.x = MAXRIGHT
+				
+			#create trash
+			if randint(0,diff) == 1:
+				newTrash = Trash(IMAGES)
+				trashes.add(newTrash)
+				
+			#redraw screen
+			windowSurface.blit(image, bgRect)
+			windowSurface.blit(text, textRect)
+			windowSurface.blit(plr, plrRect)
+			for trash in trashes.sprites():
+				trash.update()
+				windowSurface.blit(trash.trash, trash.rect)
+			
+			#check for collisions
+			for trash in trashes.sprites():
+				if trash.rect.y>300:
+					if abs(trash.rect.x-plrRect.x)<100:
+						trash.kill()
+						score += 1
+					else:
+						trash.kill()
+						lives -= 1
+					diff -= 15
+			if diff < 15:
+				diff = 50
+			print(diff)
+			
+			text1 = smallFont.render("Lives left: " + str(lives), False, BLACK)
+			textRect1 = text1.get_rect()
+			textRect1.topleft = windowRect.topleft
+			
+			text2 = smallFont.render("Score: " + str(score), False, BLACK)
+			textRect2 = text2.get_rect()
+			textRect2.topleft = windowRect.topleft
+			textRect2.y += 20
+			
+			windowSurface.blit(text1, textRect1)
+			windowSurface.blit(text2, textRect2)
+			
+			pygame.display.update()
+			
+		if lives<=0:
+			alive = False
+			
+			windowSurface.fill(BLACK)
+			text3 = smallFont.render("Game over! Score: " + str(score), False, RED)
+			textRect3 = text3.get_rect()
+			textRect3.centerx = windowRect.centerx
+			textRect3.centery = windowRect.centery
+			windowSurface.blit(text3, textRect3)
+		
+			pygame.display.update()
 		await asyncio.sleep(0)
 		
 asyncio.run(main())
-	
