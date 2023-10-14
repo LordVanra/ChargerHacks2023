83c9e0780f1f03bc1125e3a819a10164aecfa7ac
LordVanra <146479458+LordVanra@users.noreply.github.com>
2023-10-14 08:49:09 -0400

    edited header

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html>
-	<div><em><b><h1>
-	How Humans Have Utterly Destroyed Our World!
-	</h1></div></b></em>
+	<header>
+		<nav>
+			<a href="game.html"> Interactive pollution game </a>
+		</nav>
+		<div>
+			<em><b><h1>
+				How Humans Have Utterly Destroyed Our World!
+			</h1></b></em>
+		</div>
+	</header>
 	<div>
-	<h2>
-	The Pure Changes That Humans Have Contributed to on Our Earth!
-	</h2>
+		<h2>
+		The Pure Changes That Humans Have Contributed to on Our Earth!
+		</h2>
 	</div>
-
-	
+	<footer>
+		<small>
+			&copy; 2023 All rights reserved - Pollution Awareness Group
+		</small>
+	</footer>
 </html>
