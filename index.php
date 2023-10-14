0bdfd7f3f990872b7811ac9b02eadc5db5488aba
LordVanra <146479458+LordVanra@users.noreply.github.com>
2023-10-14 11:00:12 -0400

    nav bar

--- index.php
+++ index.php
@@ -5,19 +5,18 @@
 		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
-
-		<!-- Insert this where you wnat the image to go -->
-		<img src="img/causes.jpg">
-		
+		<header>
+			<nav>
+				<a href="index.html">Home</a>
+				<a href="game.html">Interactive Game</a>
+			</nav>
+			<div>
+				<em><b><h1>How Humans Have Really Made the World Into Man vs. Earth</h1></b></em>
+			</div>
+				
+		</header>
 		<div>
-			<em><b>
-				<h1>How Humans Have Really Made the World Into Man vs. Earth</h1>
-			</b></em>
-		</div>
-		<div>
-			<h2>
-				In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.	
-			</h2>
+			<h2>In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.</h2>
 		</div>
 		<div>
 			<h3><b>
@@ -35,26 +34,21 @@
 			growing as well, is not a good sign. Plastic also takes more than 400 years to decompose, which means that we can not
 			stop this problem very easily. If dire changes are not made soon, then our next generations will have to suffer.</p>
 
-		</div>	
-			
-				<div>
-					<h3><b>
-					Why is Plastic Pollution Harmful to Our Earth?
-					</h3></b>
-					
-				</div>
-		<div><p>
-		Plastic Pollution is the most harmful thing on our planet right now, but why? To answer this question, one must look deeper
+		</div>		
+		<div>
+			<h3><b>
+			Why is Plastic Pollution Harmful to Our Earth?
+			</h3></b>
+		</div>
+		<div>
+			<p>Plastic Pollution is the most harmful thing on our planet right now, but why? To answer this question, one must look deeper
 		into what plastic really is. Plastic is made up of many different esters, but two of the most vile products found in plastic's
 		"genetic variation" are Bisphenol A(BPA)and Pthalates. Let's take a look at BPA first. BPA is recognized as an "endocrine disruptor",
 		which means that BPA a compound that prevents organic hormones from securing on to each other. This chemocal compound also infects air, dust, 
 		and water in the environment. 	Even worse, BPA is linked with many cancers, most notably, breast cancer. So, as one can see, plastic is not only harmful for 
 		the environment, but also for the ones who live in the environment. That includes plants as well, because when high levels of  BPA are reached in the environment,
 		plants' ability to peform photosynthesis effieciently are reduced. Many people in our world today believe that plastic only has a small effect
-		on the environment, but it has a large one, as well as huge effects on us <em><b>humans and plants</b></em>.
-		
-							
-		</div></p>
+		on the environment, but it has a large one, as well as huge effects on us <em><b>humans and plants</b></em>.</p></div>
 		<footer>
 			<small>
 				&copy; 2023 All rights reserved - Pollution Awareness Group
