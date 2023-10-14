523e6e77fff217a60de8a461899e065453f313b3
Neil-cpu <147884651+Neil-cpu@users.noreply.github.com>
2023-10-14 09:17:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Home</title>
-		<link rel="stylesheet" type="text/css" href="style.css" />
-	</head>
-	<header>
-		<nav>
-			<a href="game.html"> Interactive pollution game </a>
-		</nav>
 		<div>
 			<em><b><h1>
 				How Humans Have Really Made the World Into Man vs. Earth
 			</h1></b></em>
-			<div><b><p>
-In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.				
-			</b></p>
 		</div>
-	</header>
+			<b><p><div><h2>
+In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.				
+			</b></p></div>
+			</h2>
 	<div>
-		<h2>
+		<h3>
 		The (Im)Pure Changes That Humans Have Contributed to on Our Earth!
-		</h2>
+		</h3>
 	</div>
+				
 	<footer>
 		<small>
 			&copy; 2023 All rights reserved - Pollution Awareness Group
