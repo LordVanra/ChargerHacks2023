7a85a716ce94db0dab191b1878be0412f9814f68
LordVanra <146479458+LordVanra@users.noreply.github.com>
2023-10-14 09:42:02 -0400

    formatting

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<title>Home</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
 		<div>
-			<em><b><h1>
-				How Humans Have Really Made the World Into Man vs. Earth
-			</h1></b></em>
+			<em><b>
+				<h1>How Humans Have Really Made the World Into Man vs. Earth</h1>
+			</b></em>
 		</div>
-			<div><h2>
-In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.				
-			</div></h2>
-	<div>
-		<h3><b>
-		Why is Plastic Pollution Happening?
-		</h3><b>
-	</div>
-		<div><p>
-			Ever since the 1960s and early 70s, our beloved Earth has been "poisoned" as one would say, 
-		in the ever-rampant mess known as plastic pollution. The cause of this mess is, well, us human beings.
-		The first thing the reader must know, however, is that plastic is an extremely cheap and easily accessible material.
-		It's cheap because it's created from the remains of oil manifacturing, making this material very cheap and it's 
-		produced in great amounts. That, and the fact that it' very versatile. It can be a grocery bag, a solid insulator, 
-		used in textiles, and much, much more. These are the main factors that propel us to use plastic in everyday life.
-		Plastic is tempting and accessible to use in all fields across the world, and the fact that our population on Earth is
-		growing as well, is not a good sign. Plastic also takes more than 400 years to decompose, which means that we can not
-		stop this problem very easily. If dire changes are not made soon, then our next generations will have to suffer.
-		</div></p>
-				
-	<footer>
-		<small>
-			&copy; 2023 All rights reserved - Pollution Awareness Group
-		</small>
-	</footer>
+		<div>
+			<h2>
+				In this article, the Pollution Awareness Group feels obligated to inform all readers, on the destructive topic of Plastic Pollution.	
+			</h2>
+		</div>
+		<div>
+			<h3><b>
+			Why is Plastic Pollution Happening?
+			</h3><b>
+		</div>
+		<div>
+			<p>Ever since the 1960s and early 70s, our beloved Earth has been "poisoned" as one would say, 
+			in the ever-rampant mess known as plastic pollution. The cause of this mess is, well, us human beings.
+			The first thing the reader must know, however, is that plastic is an extremely cheap and easily accessible material.
+			It's cheap because it's created from the remains of oil manifacturing, making this material very cheap and it's 
+			produced in great amounts. That, and the fact that it' very versatile. It can be a grocery bag, a solid insulator, 
+			used in textiles, and much, much more. These are the main factors that propel us to use plastic in everyday life.
+			Plastic is tempting and accessible to use in all fields across the world, and the fact that our population on Earth is
+			growing as well, is not a good sign. Plastic also takes more than 400 years to decompose, which means that we can not
+			stop this problem very easily. If dire changes are not made soon, then our next generations will have to suffer.</p>
+		</div>	
+		<footer>
+			<small>
+				&copy; 2023 All rights reserved - Pollution Awareness Group
+			</small>
+		</footer>
+	</body>
 </html>
