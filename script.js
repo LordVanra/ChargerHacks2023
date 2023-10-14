c2ec2f38d67701782192a5cef43861cf3f8c96f0
LordVanra <arnav.omkar@gmail.com>
2023-10-14 17:30:41 -0400

    modal code

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function gameOpened(){
 	document.getElementById("popup").innerHTML = "If you lost the game, don't be discouraged. You were meant to lose. So why did you lose the game? One committed person is not enough to fight against pollution. We need people to band together and stop pollution before it becomes too late.That is the purpose of this website. So share with all your friends and do your part to reduce pollution.";
 }
+
+var popup = document.getElementById("popup");
+var btn = document.getElementById("x");
+setTimeout(function(){popup.style.display="block";}, 5000);
+btn.onclick = function(){popup.style.display="none";}
