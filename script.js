be17f7289ec6b83eacb4aecf0629f566bcf53665
LordVanra <146479458+LordVanra@users.noreply.github.com>
2023-10-15 11:02:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function gameOpened(){
-	document.getElementById("popup").innerHTML = "If you lost the game, don't be discouraged. You were meant to lose. So why did you lose the game? One committed person is not enough to fight against pollution. We need people to band together and stop pollution before it becomes too late.That is the purpose of this website. So share with all your friends and do your part to reduce pollution.";
+	document.getElementById("popuptext").innerHTML = "If you lost the game, don't be discouraged. You were meant to lose. So why did you lose the game? One committed person is not enough to fight against pollution. We need people to band together and stop pollution before it becomes too late.That is the purpose of this website. So share with all your friends and do your part to reduce pollution.";
+	document.getElementById("popupimage").style.display = "block";
 }
 
 var popup = document.getElementById("popup");
